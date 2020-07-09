b37c3a853709336dd7794742f9089f11ca38008b
Chirag Rana <42936626+ChiragJRana@users.noreply.github.com>
2020-07-09 16:48:50 +0530

    Create the Post Model in an Sqlite Database

---
+++ blog_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# These are the database models 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default =  timezone.now) # now is a function but we do dont want to return a value we want to pass the function
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    
